4c10d298728555206a634644356bd7f5c8e0d57c
dcrawsh <dcrawsh@gmail.com>
2020-06-22 19:10:05 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,89 +1,72 @@
-document.addEventListener('DOMContentLoaded', (event)=> {
-    loadImages();
-    loadBreeds();
+let breeds = [];
+
+document.addEventListener('DOMContentLoaded', function () {
+  loadImages();
+  loadBreedOptions();
 });
 
+function loadImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(imgUrl)
+    .then(res=> res.json())
+    .then(results => {
+      results.message.forEach(image => addImage(image))
+    });
+}
 
+function addImage(dogPicUrl) {
+  let container = document.querySelector('#dog-image-container');
+  let newImageEl = document.createElement('img');
+  newImageEl.src = dogPicUrl;
+  container.appendChild(newImageEl);
+}
 
+function loadBreedOptions() {
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+  fetch(breedUrl)
+    .then(res => res.json())
+    .then(results => {
 
-    
-    function loadImages() {
-        const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-        fetch(imgUrl)
-            .then(res => res.json())
-            .then((data) => {
-                data.message.forEach((image) => {
-                addImage(image);
-                })
+      breeds = Object.keys(results.message);
+      updateBreedList(breeds);
+      addBreedSelectListener();
+    });
+}
 
-        })};
+function updateBreedList(breeds) {
+  let ul = document.querySelector('#dog-breeds');
+  removeChildren(ul);
+  breeds.forEach(breed => addBreed(breed));
+}
 
+function removeChildren(element) {
+  let child = element.lastElementChild;
+  while (child) {
+    element.removeChild(child);
+    child = element.lastElementChild;
+  }
+}
 
-    function addImage(url) {
-        let picContainer = document.querySelector('#dog-image-container');
-        let newImage = document.createElement('img');
-        newImage.src = url;
-        picContainer.appendChild(newImage);
+function selectBreedsStartingWith(letter) {
+  updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
+}
 
-    }
-    let breeds;
-    function loadBreeds() {
-        const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-        fetch(breedUrl)
-        .then(res => res.json())
-        .then((data) => {
-            breeds = Object.keys(data.message);
-            breeds.forEach((breed) => {
-                addBreedtoList(breed);
-            })
-            })
-        }
-    let list = document.getElementById('dog-breeds');
-    
-    function addBreedtoList(breed) {
-        
-        let listItem = document.createElement('li');
-        listItem.innerText = breed;
-        list.appendChild(listItem);
-    }
+function addBreedSelectListener() {
+  let breedDropdown = document.querySelector('#breed-dropdown');
+  breedDropdown.addEventListener('change', function (event) {
+    selectBreedsStartingWith(event.target.value);
+  });
+}
 
+function addBreed(breed) {
+  let ul = document.querySelector('#dog-breeds');
+  let li = document.createElement('li');
+  li.innerText = breed;
+  li.style.cursor = 'pointer';
+  ul.appendChild(li);
+  li.addEventListener('click', updateColor);
+}
 
-    
-    list.addEventListener('click', (event) => {
-       event.target.style.color = 'green';
-    })
-
-    
-    let dropDown = document.querySelector('#breed-dropdown');
-        dropDown.addEventListener('change', (event) => {
-        filterBreeds(event.target.value);
-    })
-
-    
-    //how to reload list breeds so it can filter more than
-    function filterBreeds(choice) {
-        let filteredBreeds = breeds.filter(breed => breed.startsWith(choice))
-        updateBreedList();
-        filteredBreeds.forEach(breed => addBreedtoList(breed))  
-    } 
-
-    function updateBreedList() {
-        removeChildren(list);
-    }
-
-    function removeChildren(list) {
-        let child = list.lastElementChild;
-        while (child) {
-            list.removeChild(child);
-            child = list.lastElementChild;
-        }
-    }
-
-
-    
-                   
-
-
-
-
-
+function updateColor(event) {
+  event.target.style.color = 'palevioletred';
+}
